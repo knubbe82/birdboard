9cc78ccb493ee156e625e1f52fa0622cf5d5bf17
knubbe <knubbe1982@gmail.com>
2019-01-20 16:02:36 +0100

    styling the project page

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -1,7 +1,34 @@
 @extends('layouts.app')
 
 @section('content')
-	<h1>{{ $project->title}}</h1>
-	<div>{{ $project->description }}</div>
-	<a href="/projects">Go Back</a>
+	<header class="flex items-center mb-3 py-4">
+		<div class="flex justify-between items-end w-full">
+			<p class="text-grey text-sm font-normal">
+				<a href="/projects" class="text-grey text-sm font-normal no-underline">My Projects</a> / {{ $project->title }}
+			</p>
+			<a href="/projects/create" class="button">New Project</a>
+		</div>
+	</header>
+
+	<main>
+		<div class="lg:flex -mx-3">
+			<div class="lg:w-3/4 px-3 mb-6">
+				<div class="mb-8">
+					<h2 class="text-lg text-grey font-normal mb-3">Tasks</h2>
+					<div class="card mb-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi distinctio officia eius reiciendis deleniti laudantium exercitationem, veniam asperiores quas nihil rem ipsa eveniet impedit perferendis, doloremque nisi vero dolore. Ipsam.</div>
+				</div>
+				<div>
+					<h2 class="text-lg text-grey font-normal mb-3">General Notes</h2>
+					<textarea class="card w-full" style="min-height: 200px">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fuga ut distinctio, repellendus exercitationem quae aperiam nihil veritatis pariatur nam. Quia quasi ex perferendis nesciunt accusantium suscipit, aperiam ad a ea!</textarea>
+				</div>
+			</div>
+			
+		
+			<div class="lg:w-1/4 px-3">
+				@include('projects.card')
+			</div>
+		</div>
+	</main>
+
+	
 @endsection
